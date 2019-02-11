5a702574c7c9128652c3bf294e1e1be0efdf2bcf
Ivan Badbaden <ivanbadbaden@gmail.com>
2019-02-11 08:51:00 +0800

    Initial commit

---
+++ resources/views/layouts/headers/guest.blade.php
@@ -0,0 +1,20 @@
+<div class="header bg-gradient-primary py-7 py-lg-6">
+    <div class="container">
+        <div class="header-body text-center mb-7">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 col-md-6">
+                    <h1 class="text-white">{{ __('Wireless Access for Health') }}</h1>
+                    <h1 class="text-white">{{ __('Inventory Management System') }}</h1>
+                    <!-- <p class="text-lead text-light">
+                        This is a project made by the students of <strong>Tarlac State University - College of Computer Studies</strong>
+                    </p> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="separator separator-bottom separator-skew zindex-100">
+        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+    </div>
+</div>
